918240e6aa7699ef4b98b862b825db4e1cfdf39b
Maksim Moryakov <maksim.moryakov44@yandex.ru>
2023-09-01 10:39:15 +0300

    Update-1

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -113,13 +113,13 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.IsAuthenticated',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
